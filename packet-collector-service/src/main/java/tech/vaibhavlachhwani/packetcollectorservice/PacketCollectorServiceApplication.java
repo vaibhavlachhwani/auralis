be706ede39465a460f05492d398d0ac626ad40ce
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2025-08-26 01:50:53 +0530

    fixes.

---
+++ packet-collector-service/src/main/java/tech/vaibhavlachhwani/packetcollectorservice/PacketCollectorServiceApplication.java
@@ -0,0 +1,13 @@
+package tech.vaibhavlachhwani.packetcollectorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacketCollectorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PacketCollectorServiceApplication.class, args);
+    }
+
+}
---
+++ packet-collector-service/src/test/java/tech/vaibhavlachhwani/packetcollectorservice/PacketCollectorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.vaibhavlachhwani.packetcollectorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacketCollectorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
