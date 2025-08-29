bcc2a44f1367bef3d8dfd24d7c39d35ed373ea5e
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2025-08-29 07:25:59 +0530

    implement basic packet ingestion handler

--- packet-collector-service/src/main/java/tech/vaibhavlachhwani/packetcollectorservice/handlers/PacketIngestHandler.java
+++ packet-collector-service/src/main/java/tech/vaibhavlachhwani/packetcollectorservice/handlers/PacketIngestHandler.java
@@ -1,16 +1,30 @@
 package tech.vaibhavlachhwani.packetcollectorservice.handlers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+import tech.vaibhavlachhwani.packetcollectorservice.dto.PacketMetrics;
+import tech.vaibhavlachhwani.packetcollectorservice.service.RealTimeMetricsAggregator;
 
 @Component
+@AllArgsConstructor
 public class PacketIngestHandler extends TextWebSocketHandler {
+    private final RealTimeMetricsAggregator aggregator;
+
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        String recMessage = message.getPayload();
-        System.out.println("Received : " + recMessage);
+        String packetMetricsJsonString = message.getPayload();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        PacketMetrics packetMetrics = objectMapper.readValue(packetMetricsJsonString, PacketMetrics.class);
+
+//        System.out.println(packetMetrics);
+//        System.out.println("===============================================================================");
+        aggregator.processPacket(packetMetrics);
+
         TextMessage response = new TextMessage("Server ACK");
         session.sendMessage(response);
     }
