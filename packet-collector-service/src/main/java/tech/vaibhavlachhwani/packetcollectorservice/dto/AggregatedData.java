4a939a96186409bb92200b3d2c9f78c9c2c4ca38
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2025-08-29 07:25:06 +0530

    make additional dtos

--- packet-collector-service/src/main/java/tech/vaibhavlachhwani/packetcollectorservice/dto/AggregatedData.java
+++ packet-collector-service/src/main/java/tech/vaibhavlachhwani/packetcollectorservice/dto/AggregatedData.java
@@ -1,4 +1,24 @@
 package tech.vaibhavlachhwani.packetcollectorservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class AggregatedData {
+    private long totalBytes;
+    private long totalPackets;
+    private long newConnectionAttemptCount;
+    private long connectionResetCount;
+
+    private Map<String, Long> bytesPerProtocol;
+    private Map<String, Long> bytesBySourceIp;
+    private Map<String, Long> bytesByDestinationIp;
+    private Map<Integer, Long> bytesPerDestinationPort;
 }
--- packet-collector-service/src/main/java/tech/vaibhavlachhwani/packetcollectorservice/dto/DashboardData.java
+++ packet-collector-service/src/main/java/tech/vaibhavlachhwani/packetcollectorservice/dto/DashboardData.java
@@ -1,4 +1,44 @@
 package tech.vaibhavlachhwani.packetcollectorservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class DashboardData {
+    private double bandwidthMbps;
+    private double packetsPerSecond;
+    private double averagePacketSizeBytes;
+
+    private double newConnectionsPerSecond;
+    private double resetsPerSecond;
+
+    private Map<String, Long> protocolDistribution;
+
+    private List<IpTraffic> topTalkers;
+    private List<IpTraffic> topDestinations;
+    private List<PortTraffic> topServices;
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class IpTraffic {
+        private String ip;
+        private long bytes;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class PortTraffic {
+        private int port;
+        private long bytes;
+    }
 }
