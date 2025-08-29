36d0f257d2884593900a55d3784ac502580158a3
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2025-08-29 07:28:31 +0530

    enable scheduling

--- packet-collector-service/src/main/java/tech/vaibhavlachhwani/packetcollectorservice/PacketCollectorServiceApplication.java
+++ packet-collector-service/src/main/java/tech/vaibhavlachhwani/packetcollectorservice/PacketCollectorServiceApplication.java
@@ -2,8 +2,10 @@ package tech.vaibhavlachhwani.packetcollectorservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PacketCollectorServiceApplication {
 
     public static void main(String[] args) {
