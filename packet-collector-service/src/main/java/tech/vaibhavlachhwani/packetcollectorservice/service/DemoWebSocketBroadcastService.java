465b1a6a948f9640e59e459fd541e7207b52c51f
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2025-09-06 01:53:08 +0530

    remove sockjs fallback

---
+++ packet-collector-service/src/main/java/tech/vaibhavlachhwani/packetcollectorservice/service/DemoWebSocketBroadcastService.java
@@ -0,0 +1,17 @@
+package tech.vaibhavlachhwani.packetcollectorservice.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+import tech.vaibhavlachhwani.packetcollectorservice.dto.DashboardData;
+
+@Service
+@AllArgsConstructor
+public class DemoWebSocketBroadcastService {
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public void broadcast(DashboardData data) {
+        messagingTemplate.convertAndSend("/topic/demo", data.getBandwidthMbps());
+        System.out.println("Sending... " + data.getBandwidthMbps());
+    }
+}
