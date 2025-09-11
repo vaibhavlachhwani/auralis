d521eb2bb55a8bf7bf53f8879480111ad94d85c3
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2025-09-11 12:23:59 +0530

    improve site header

--- dashboard/src/components/site-header/SiteHeader.tsx
+++ dashboard/src/components/site-header/SiteHeader.tsx
@@ -4,7 +4,7 @@ import { ThemePicker } from "./ThemePicker";
 
 export function SiteHeader({ title }: { title: string }) {
   return (
-    <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+    <header className="flex p-1 h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
         <SidebarTrigger className="-ml-1" />
         <Separator
