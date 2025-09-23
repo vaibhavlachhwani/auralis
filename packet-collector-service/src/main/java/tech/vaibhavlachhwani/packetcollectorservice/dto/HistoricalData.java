5b6f1fbf2722890e1955dc87c3da4019ac9f6b24
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2025-09-23 22:19:43 +0530

    create HistoricalData dto

--- packet-collector-service/src/main/java/tech/vaibhavlachhwani/packetcollectorservice/dto/HistoricalData.java
+++ packet-collector-service/src/main/java/tech/vaibhavlachhwani/packetcollectorservice/dto/HistoricalData.java
@@ -1,4 +1,37 @@
 package tech.vaibhavlachhwani.packetcollectorservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Map;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class HistoricalData {
+    private Instant queryStart;
+    private Instant queryEnd;
+
+    private List<TrendDataPoint> trendData;
+
+    private Map<String, Long> protocolDistribution;
+
+    private List<DashboardData.IpTraffic> topTalkers;
+    private List<DashboardData.IpTraffic> topDestinations;
+    private List<DashboardData.PortTraffic> topServices;
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class TrendDataPoint {
+        private Instant time;
+        private double avgBandwidth;
+        private double maxBandwidth;
+    }
 }
